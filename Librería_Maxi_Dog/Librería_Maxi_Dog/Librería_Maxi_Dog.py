e5e162dfe0c285939c60770e5f19773f507db4b3
Funkalux <31772944+Funkalux@users.noreply.github.com>
2017-09-19 15:08:21 -0500

    Funcion suma

--- Librería_Maxi_Dog/Librería_Maxi_Dog/Librería_Maxi_Dog.py
+++ Librería_Maxi_Dog/Librería_Maxi_Dog/Librería_Maxi_Dog.py
@@ -1,7 +1,7 @@
 
 print ("Bienvenido a la librería Maxi Dog")
 print ("-"*80+  "\n")
-print ("¿Qué es lo que deseas hacer el día de hoy? Introduce el nombre de la función y después aprieta enter")
+print ("¿Qué es lo que deseas hacer el día de hoy? \n\nIntroduce el nombre de la función y después aprieta enter\n")
 
 print("Suma")
 print("Resta")
@@ -10,14 +10,21 @@ print("División")
 print("Potencia")
 print("Raíz")
 print("Módulo")
-print("Conversión de binario a hexadecimal")
-print("Conversión de hexadecimal a binario")
-print("Conversión de decimal a hexadecimal")
-print("Conversión de hexadecimal a decimal")
-print("Conversión de binario a decimal")
-print("Conversión de decimal a binario")
-print("Metros a Yardas")
+print("Binario a hexadecimal")
+print("Hexadecimal a binario")
+print("Decimal a hexadecimal")
+print("Hexadecimal a decimal")
+print("Binario a decimal")
+print("Decimal a binario")
+print("Metros a yardas")
 print("Metros a pulgadas")
 
 operacion=input()
 operacion=str.lower(operacion)
+a=int(input())
+b=int(input())
+
+def suma():
+    c=a+b
+    return c
+print(suma())
