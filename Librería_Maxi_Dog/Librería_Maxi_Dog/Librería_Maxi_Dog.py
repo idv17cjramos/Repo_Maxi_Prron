75ef4de7b4eb048218e7abda0ab4d96cc90d71de
idv17c-Julian-Gil <31995069+idv17c-Julian-Gil@users.noreply.github.com>
2017-09-19 18:32:21 -0500

    Menu toshuu toguapo
    menu toshuu toguapo

--- Librería_Maxi_Dog/Librería_Maxi_Dog/Librería_Maxi_Dog.py
+++ Librería_Maxi_Dog/Librería_Maxi_Dog/Librería_Maxi_Dog.py
@@ -1,8 +1,10 @@
 
+#Bienvenido Saint Yeipi
+ 
 print ("Bienvenido a la librería Maxi Dog")
 print ("-"*80+  "\n")
 print ("¿Qué es lo que deseas hacer el día de hoy? \n\nIntroduce el nombre de la función y después aprieta enter\n")
-
+ 
 print("Suma")
 print("Resta")
 print("Multiplicación")
@@ -19,44 +21,45 @@ print("Decimal a binario")
 print("Metros a yardas")
 print("Metros a pulgadas")
 print ("-"*80+  "\n")
-
-
+ 
+operacion=input("Que operacion quieres realizar? ")
 operacion=str.lower(operacion)
-separator = " "
 
+
+ 
 def suma():
-
-    # Aqui se hace la machaca *pas pas*
     sumita = input("Introduce numeros: ")
-    sumita = [int(x) for x in sumita.split (" ") ] 
-    total = 0 
+    sumita = [int(x) for x in sumita.split (" ") ]
+    total = 0
     for numero in sumita:
         total += numero
     print ("La suma es:", total)
-
-    numeros=int(input("Introduce los numeros que quieras sumar, separados por un espacio: \n"))
-    numerosSeparados=[numeros.split(" ")]
-    return (numerosSeparados)
-print (suma())
-#    numeros =  int(input("Escribe los numeros que quieres sumar, separados por un espacio: \n").split(separator(" ")))
-#    print(numeros)
-#    print ("The sum of numbers is: ")+str(sum(numeros))
-
-
+ 
 def resta():
-    # Aqui se hace la machaca *pas pas*
     restita = input("Introduce numeros: ")
-    restita = [int(x) for x in restita.split (" ") ] 
-    total = 0 
+    restita = [int(x) for x in restita.split (" ") ]
+    total = 0
     for numero in restita:
         total = numero
     print ("La resta es:", total)
+ 
+def multiplicacion():
+    x  =  int(input("Escribe el primer número que quieres multiplicar: "))
+    y  =  int(input("Escribe el segundo número que quieres multiplicar: "))
+    print (x*y)
+  
+def division():
+    x  =  int(input("Escribe el dividendo: "))
+    y  =  int(input("Escribe el divisor: "))
+    print (x/y)
+ 
 
-def multiplicación():
-    x  =  int(input("Escribe el primer número que quieres multiplicar"))
-    y  =  int(input("Escribe el segundo número que quieres multiplicar"))
-    x*y = resultado
-    print (resultado)
 
-
-def división():
+if (operacion== "suma"):
+    suma()
+elif(operacion=="multiplicacion"):
+    multiplicacion()
+elif (operacion == "division" ):
+    division()
+elif (operacion == "guebito con cacsun" ):
+    print("Estas reprobado para toda la vida :)")
